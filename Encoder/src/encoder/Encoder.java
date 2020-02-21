2852619f48a8f99f4fa005ccab44471c53e3086e
rorymackin <rorymackin12@outlook.com>
2020-02-21 13:32:10 +1030

    Improved the encodeFile and decodeFile methods by splitting up repeated sections of code. Improved the readFile method by using a BufferedReader instead. Testing shows there are no longer random characters introduced into the encodedFile. Created the writeFile method that writes a file to specified path.

--- Encoder/src/encoder/Encoder.java
+++ Encoder/src/encoder/Encoder.java
@@ -15,30 +15,46 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
 /**
  *
  * @author Rory Mackin
  */
 public class Encoder {
-    
-   /* 
+
+    /* 
     The queue used for encryption
-    */
+     */
     Queue<Integer> repeatingKey = new LinkedList<>();
     int[] initialKey = {2, 2, 2, 2, 2};
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
-    public String encodeMessage (String message) {
-       repeatingKey.clear();
+
+    public String readFile(String fileName) throws IOException {
+        /*
+        This method is used to read files. It creates a BufferedReader
+        It reads each line of the file into a String and the returns that String.
+        */
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String readFile = br.readLine();
+        return readFile;
+    }
+
+    public void writeFile(Path file, List decodedFile) throws IOException {
+        
+        Files.write(file, decodedFile);
+
+    }
+
+    public String encodeMessage(String message) {
         /*
         This method is the overall method used for encryption.
-        */
+         */
+        
         for (int keyVal : initialKey) {
             repeatingKey.offer(keyVal);
         }
@@ -57,22 +73,21 @@ public class Encoder {
             // added shift back into the queue
             repeatingKey.offer(shift);
             //System.out.println("key: " + repeatingKey);
-            
+
         }
         repeatingKey.clear();
         String encoded = sb.toString();
         return encoded;
-        
+
     }
-    
-    public String decodeMessage (String message) {
-        repeatingKey.clear();
+
+    public String decodeMessage(String message) {
         /*
-        This method is the overall method used for encryption.
-        */
+        This method is the overall method used for decryption.
+         */
+        
         for (int keyVal : initialKey) {
             repeatingKey.offer(keyVal);
-            // System.out.println("key: " + repeatingKey);
         }
         StringBuilder decodedBuffer = new StringBuilder();
         String decode = message;
@@ -88,52 +103,36 @@ public class Encoder {
 
             // added shift back into the queue
             repeatingKey.offer(shift);
-            //System.out.println("key: " + repeatingKey);
-            
+           
+
         }
-        
-        String decoded = decodedBuffer.toString(); 
-        
+        repeatingKey.clear();
+        String decoded = decodedBuffer.toString();
         return decoded;
-        
+
     }
 
-    public String encodeFile(String fileName) throws IOException {
-     /* 
+    public void encodeFile(String fileName) throws IOException {
+        /* 
       This encodeFile method is the first attempt at making a method that reads a file and encodes it's contents.
-     */
-        
-        //Path path = FileSystems.getDefault().getPath(fileName);
-        List lines = Files.readAllLines(Paths.get(fileName));
-        String encodedFile = lines.toString();
-        String encodedMessage2 = encodeMessage(encodedFile);
-        List <String> encodedFile2 = new ArrayList<>(Arrays.asList(encodedMessage2));
+         */
+        String message = readFile(fileName);
+        String encodedMessage = encodeMessage(message);
+        ArrayList<String> encodedFile = new ArrayList<>(Arrays.asList(encodedMessage));
         Path file = Paths.get("data" + File.separator + "hello(encoded).txt");
-        Files.write(file, encodedFile2);
-        
-        
-       
-        return "";
+        writeFile(file, encodedFile);
 
     }
-    
-    public String decodeFile(String fileName) throws IOException {
-     /* 
+
+    public void decodeFile(String fileName) throws IOException {
+        /* 
       This encodeFile method is the first attempt at making a method that reads a file and encodes it's contents.
-     */
-       
-        //Path path = FileSystems.getDefault().getPath(fileName);
-        List lines = Files.readAllLines(Paths.get(fileName));
-        String decodedFile = lines.toString();
-        String decodedMessage2 = decodeMessage(decodedFile);
-        
-        List <String> decodedFile2 = new ArrayList<>(Arrays.asList(decodedMessage2));
+         */
+        String message = readFile(fileName);
+        String encodedMessage = decodeMessage(message);
+        ArrayList<String> encodedFile = new ArrayList<>(Arrays.asList(encodedMessage));
         Path file = Paths.get("data" + File.separator + "hello(decoded).txt");
-        Files.write(file, decodedFile2);
-       
-        
-        
-        return "";
+        writeFile(file, encodedFile);
 
     }
 }
